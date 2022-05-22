e4368c097f9c25c4cefc703708522cff60f5c1e9
Iqbal <iqbal.du.mis@gmail.com>
2022-05-23 01:32:30 +0600

    About US done

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import warehouse from '../../assets/img/warehouse.jpg'
 const AboutUs = () => {
     return (
         <div className='container mt-5'>
-            <h3>About Us</h3>
-            <div className='d-flex'>
+            <h3 className='text-danger mb-5'>About Us</h3>
+            <div className='d-flex flex-md-column flex-lg-row'>
                 <div className='text-start'>
                     <h4>The Company</h4>
                     <p>RIM Electric Tools is established in 2008, with 20 years experience in tools business. FIXTECH is our registered brand. One-stop tools station, including full line of power tools, hand tools, bench tools, air tools, welding machine, water pumps, generators, garden tools and power accessories etc.</p>
@@ -13,6 +13,9 @@ const AboutUs = () => {
                     <h4>Warehouse</h4>
                     <p>We have over 5000 Square Meter warehouse. Full range of quality tools. We have Ready Stock for Rapid Delivery. We provide 7 dyas lead time.</p>
                 </div>
+            </div>
+            <div className='my-5'>
+                <img className='img-fluid' src={warehouse} alt="" />
             </div>
         </div>
     );
--- src/components/BusinessSummary/BusinessSummary.js
+++ src/components/BusinessSummary/BusinessSummary.js
@@ -6,14 +6,14 @@ const BusinessSummary = () => {
     return (
         <div className='container mt-5'>
             <h2 className='mb-5 text-danger'>Business Summary</h2>
-            <div className='d-flex justify-content-center'>
-                <div className='text-start px-5'>
+            <div className='d-flex flex-md-column flex-lg-row justify-content-center'>
+                <div className='text-start mb-5'>
                     <h4 > <FontAwesomeIcon icon={faCheck} /> We Manufactured over 30+ Tools</h4>
                     <h4> <FontAwesomeIcon icon={faCheck} /> 80+ countries all over the World</h4>
                     <h4> <FontAwesomeIcon icon={faCheck} /> 1000+ Satisfied Customer</h4>
                     <h4> <FontAwesomeIcon icon={faCheck} /> ISO 1000:9000 Certification</h4>
                 </div>
-                <div className='text-start px-5'>
+                <div className='text-start mb-5'>
                     <h4 > <FontAwesomeIcon icon={faCheck} /> 20 Years in Business</h4>
                     <h4> <FontAwesomeIcon icon={faCheck} /> Participated 50+ International Trade Fair</h4>
                     <h4> <FontAwesomeIcon icon={faCheck} /> Multiple manufacturing plant</h4>
